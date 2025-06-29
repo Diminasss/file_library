52c7ff348cb1c46b782fd89225c49e462210be78
Diminasss <pilluping@gmail.com>
2025-06-30 01:02:43 +0300

    Первый коммит

---
+++ lib/filelib.cpp
@@ -0,0 +1,2 @@
+#include "filelib.h"
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include "lib/filelib.h" // Включение заголовочного файла с определением класса FileLib
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
